c8cf4da77668c028bfd3f948ae54231172732928
SlyShy <mkbunday@stanford.edu>
2009-12-13 16:08:58 -0600

    Solved problem 3.

---
+++ problem_3.rb
@@ -0,0 +1,19 @@
+def largest_prime_factor num
+  a = first_factor num
+  if a == :prime
+    return num
+  else
+    return [largest_prime_factor(a), largest_prime_factor(num / a)].max
+  end
+end
+
+def first_factor num
+  for i in (2..Math.sqrt(num).floor)
+    return i if num % i == 0
+  end
+  return :prime
+end
+
+class Main
+  puts largest_prime_factor 600851475143 
+end
