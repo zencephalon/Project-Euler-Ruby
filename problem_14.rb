7ee3c3ecc74edd119a4f3f0973b57090689bb13f
root <mathwizard1232@gmail.com>
2009-12-20 16:16:21 -0600

    7

---
+++ problem_14.rb
@@ -0,0 +1,27 @@
+class Main
+  answers = [0]
+  for i in (1..1000000-1)
+#  for i in (1..13)
+#    puts i
+    term = i
+    length = 1
+    solved = 0
+    while term != 1 && solved == 0
+      if term < i
+        length += answers[term] - 1
+        solved = 1
+      else
+        if term % 2 == 0
+          term /= 2
+        else
+          term = 3*term + 1
+        end
+        length += 1
+      end
+    end
+#    puts length
+    answers << length
+  end
+
+  puts answers.each_with_index.max[1]
+end
